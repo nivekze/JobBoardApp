ff74d533d3de0a90cb78c349bdb53154ac8046df
kevin_alvarez93 <omar.alher93@gmail.com>
2020-04-21 20:43:50 -0600

    Removing files from default start up, adding Job controller

---
+++ JobBoardApp/Controllers/JobController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBoardApp.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Job/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Job/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Job/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Job/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Job/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Job/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Job/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
