83c1ef92fc76545781d0503a1a7bc08b11d7d501
kevin_alvarez93 <omar.alher93@gmail.com>
2020-04-21 18:31:09 -0600

    Init new clean project

---
+++ JobBoardApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobBoardApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
