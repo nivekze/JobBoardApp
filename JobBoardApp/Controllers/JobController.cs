946f5c4d23b0e719cf3d9a0b0c5411f1ed0793ee
kevin_alvarez93 <omar.alher93@gmail.com>
2020-04-21 22:49:44 -0600

    Job controller, adding dt with ajax, create job and init edit job

--- JobBoardApp/Controllers/JobController.cs
+++ JobBoardApp/Controllers/JobController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobBoardApp.Models;
+using JobBoardApp_Infrastructure;
+using JobBoardApp_Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +12,11 @@ namespace JobBoardApp.Controllers
 {
     public class JobController : Controller
     {
+        private readonly IJobRepository _jobRepository;
+        public JobController(IJobRepository jobRepository)
+        {
+            _jobRepository = jobRepository;
+        }
         // GET: Job
         public IActionResult Index()
         {
@@ -24,36 +32,63 @@ namespace JobBoardApp.Controllers
         // GET: Job/Create
         public IActionResult Create()
         {
-            return View();
+            ViewBag.PageTitle = "Create Job";
+            return View("NewEdit", new JobViewModel());
         }
 
         // POST: Job/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(IFormCollection collection)
+        public IActionResult Create(JobViewModel job)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (!ModelState.IsValid) 
+                {
+                    ViewBag.Error = "Required data is missing!";
+                    return View("NewEdit", job);
+                }
+                _jobRepository.CreateJob(new JobDTO {
+                    Title = job.Title,
+                    Description = job.Description,
+                    ExpiresAt = job.ExpiresAt
+                });
 
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View("NewEdit", job);
             }
         }
 
         // GET: Job/Edit/5
         public IActionResult Edit(int id)
         {
-            return View();
+            try 
+            {
+                var job = _jobRepository.GetById(id);
+                return View("NewEdit", new JobViewModel
+                {
+                    Id = job.Id,
+                    Title = job.Title,
+                    Description = job.Description,
+                    ExpiresAt = job.ExpiresAt
+                });
+            }
+            catch (Exception ex) 
+            {
+                ViewBag.Error = ex.Message;
+                return View("NewEdit", new JobViewModel());
+            }
+            
         }
 
         // POST: Job/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(int id, JobViewModel job)
         {
             try
             {
@@ -87,6 +122,31 @@ namespace JobBoardApp.Controllers
             catch
             {
                 return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetJobPaginated([FromQuery]int? draw, [FromQuery] int? start, [FromQuery] int? length, [FromQuery] string search, [FromQuery] int? page, [FromQuery] string order = "desc")
+        {
+            search = search ?? Request.Query["search[value]"].ToString();
+            order = Request.Query["order[0][dir]"].ToString() ?? order;
+            length = length ?? 10;
+            var totalRecords = 0;
+            var recordsFiltered = 0;
+            start = start.HasValue ? start / length : 0;
+            start = page.HasValue ? page - 1 : start;
+
+            try
+            {
+                var list = _jobRepository.GetPaginated(search, start.Value, length.Value, order, out totalRecords, out recordsFiltered);
+
+                return new JsonResult(
+                    new { draw, start, length, totalRecords, recordsFiltered, data = list }
+                    );
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e.Message);
             }
         }
     }
---
+++ JobBoardApp_Models/DTO/JobDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoardApp_Models.DTO
+{
+    public class JobDTO
+    {
+        public int? Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
